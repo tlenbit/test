577aa7c99dc5d4221b559174862f349076bc3a36
markov <i.markov@group-ib.com>
2019-04-06 19:11:20 +0300

    add task5 classes and IDs support

--- json_to_html.py
+++ json_to_html.py
@@ -1,14 +1,36 @@
 import json
+import re
 from collections import OrderedDict
 
-def get_element(tag, inner_html):
-    return f'<{tag}>{inner_html}</{tag}>'
+
+def get_element(tag, inner_html, id='', classes=[]):
+    id_part = f' id="{id}"' if id else ''
+    classes_part = f' class="{" ".join(classes)}"' if classes else ''
+    return f'<{tag}{id_part}{classes_part}>{inner_html}</{tag}>'
+
+def parse_key(key):
+    ''' Parse key of the form: "p.my-class1#my-id.my-class2" '''
+    if '#' in key:
+        part1, part2, *_ = key.split('#')
+        if _:
+            raise Exception('Error: more than one id has been provided')
+        tag, *classes1 = part1.split('.')
+        id, *classes2 = part2.split('.')
+        return {'tag': tag, 'id': id, 'classes': classes1 + classes2}
+
+    if '.' in key:
+        tag, *classes = key.split('.')
+        return {'tag': tag, 'id': '', 'classes': classes}
+
+    return {'tag': key, 'id': '', 'classes': []}
 
 def json_to_html(source):
     if type(source) == str:
         return source
+
     if type(source) == OrderedDict:
-        return ''.join([get_element(tag, json_to_html(source[tag])) for tag in source])
+        return ''.join([get_element(**parse_key(key), inner_html=json_to_html(source[key])) for key in source])
+
     if type(source) == list:
         return get_element('ul', ''.join([get_element('li', json_to_html(el)) for el in source]))
 
